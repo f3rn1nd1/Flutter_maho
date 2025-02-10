83dc45f168976e62a5b731c155230ed9b7bd6af2
f3rn1nd1 <mby301101@gmail.com>
2025-02-10 15:41:11 -0300

    feat se integra segmetedButton para usuarios activos y elimindos

--- lib/view/search.dart
+++ lib/view/search.dart
@@ -15,6 +15,7 @@ class SearchTableState extends State<SearchTable> {
   final TextEditingController _searchController = TextEditingController();
   List<User> _filteredUsers = [];
   int _currentPage = 1;
+  String _selectedView = 'active';
 
   void _onSearchPressed() {
     String query = _searchController.text.trim();
@@ -24,15 +25,23 @@ class SearchTableState extends State<SearchTable> {
   @override
   void initState() {
     super.initState();
+    _loadUsers();
+  }
 
+  void _loadUsers() {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     final isAdmin = userProvider.currentUser?.admin == 1;
 
-    // Cargar los usuarios de acuerdo con el rol
     if (isAdmin) {
-      userProvider.getUsers(page: _currentPage).then((_) {
-        _filteredUsers = userProvider.users;
-      });
+      if (_selectedView == 'active') {
+        userProvider.getUsers(page: _currentPage).then((_) {
+          _filteredUsers = userProvider.users;
+        });
+      } else {
+        userProvider.getTrashUsers(page: _currentPage).then((_) {
+          _filteredUsers = userProvider.users;
+        });
+      }
     } else {
       userProvider.infoUsers().then((_) {
         _filteredUsers = userProvider.users;
@@ -52,15 +61,21 @@ class SearchTableState extends State<SearchTable> {
 
     setState(() {
       if (query.isEmpty) {
-        // Si la consulta está vacía, carga la primera página sin filtro
         _loadPage(1);
       } else {
-        // Si hay una consulta, llama a getUsers con el parámetro search
-        userProvider.getUsers(page: 1, search: query).then((_) {
-          setState(() {
-            _filteredUsers = userProvider.users;
+        if (_selectedView == 'active') {
+          userProvider.getUsers(page: 1, search: query).then((_) {
+            setState(() {
+              _filteredUsers = userProvider.users;
+            });
           });
-        });
+        } else {
+          userProvider.getTrashUsers(page: 1, search: query).then((_) {
+            setState(() {
+              _filteredUsers = userProvider.users;
+            });
+          });
+        }
       }
     });
   }
@@ -68,12 +83,21 @@ class SearchTableState extends State<SearchTable> {
   // Función para manejar la paginación
   void _loadPage(int page) {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
-    userProvider.getUsers(page: page).then((_) {
-      setState(() {
-        _filteredUsers = userProvider.users;
-        _currentPage = page;
+    if (_selectedView == 'active') {
+      userProvider.getUsers(page: page).then((_) {
+        setState(() {
+          _filteredUsers = userProvider.users;
+          _currentPage = page;
+        });
       });
-    });
+    } else {
+      userProvider.getTrashUsers(page: page).then((_) {
+        setState(() {
+          _filteredUsers = userProvider.users;
+          _currentPage = page;
+        });
+      });
+    }
   }
 
   @override
@@ -94,126 +118,147 @@ class SearchTableState extends State<SearchTable> {
         Map<String, dynamic> userData = snapshot.data ?? {};
         bool isAdmin = userData['admin']?.toString() == "1";
 
-        return
-          Card(
-            elevation: 5,
-            margin: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _searchController,
-                          decoration: InputDecoration(
-                            labelText: 'Buscar',
-                            hintText: 'Ingrese un nombre',
-                            prefixIcon: const Icon(Icons.search),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
+        return Card(
+          elevation: 5,
+          margin: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          labelText: 'Buscar',
+                          hintText: 'Ingrese un nombre',
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
                           ),
-                          onChanged: (value) {
-                            // Opcional: Si quieres que busque en tiempo real al escribir, mantenlo
-                          },
                         ),
+                        onChanged: (value) {
+                          // Opcional: Si quieres que busque en tiempo real al escribir, mantenlo
+                        },
                       ),
-                      const SizedBox(width: 8), // Espaciado entre el campo de texto y el botón
-                      ElevatedButton(
-                        onPressed: _onSearchPressed,
-                        child: const Text("Buscar"),
+                    ),
+                    const SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: _onSearchPressed,
+                      child: const Text("Buscar"),
+                    ),
+                  ],
+                ),
+              ),
+              // Agregar el SegmentedButton aquí
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SegmentedButton<String>(
+                  segments: const [
+                    ButtonSegment(value: 'active', label: Text('Activos')),
+                    ButtonSegment(value: 'trash', label: Text('Eliminados')),
+                  ],
+                  selected: <String>{_selectedView},
+                  onSelectionChanged: (Set<String> newSelection) {
+                    setState(() {
+                      _selectedView = newSelection.first;
+                      _loadUsers(); // Recargar usuarios según la selección
+                    });
+                  },
+                ),
+              ),
+              if (userProvider.isLoading)
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: CircularProgressIndicator(),
+                )
+              else if (_filteredUsers.isEmpty)
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'No hay usuarios disponibles.',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                )
+              else
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTable(
+                        columns: const [
+                          DataColumn(label: Text("")),
+                          DataColumn(label: Text("Nombre")),
+                          DataColumn(label: Text("Correo")),
+                          DataColumn(label: Text("Teléfono")),
+                          DataColumn(label: Text("Anexo")),
+                          DataColumn(label: Text("Acciones")),
+                        ],
+                        rows: _filteredUsers.map((user) {
+                          return DataRow(cells: [
+                            DataCell(
+                              IconButton(
+                                icon: const Icon(Icons.add_circle,
+                                    color: Colors.blue),
+                                onPressed: () => showInfoModal(context, user),
+                              ),
+                            ),
+                            DataCell(SelectableText(user.name.toString())),
+                            DataCell(SelectableText(user.email.toString())),
+                            DataCell(SelectableText(user.telefono.toString())),
+                            DataCell(SelectableText(user.anexo.toString())),
+                            DataCell(
+                              Row(
+                                children: [
+                                  if (isAdmin)
+                                    IconButton(
+                                      icon: const Icon(Icons.edit,
+                                          color: Colors.blue),
+                                      onPressed: () =>
+                                          showEditModal(context, user),
+                                    ),
+                                  if (isAdmin)
+                                    IconButton(
+                                      icon: const Icon(Icons.delete,
+                                          color: Colors.red),
+                                      onPressed: () => userProvider
+                                          .deleteUser(user.id!.toInt()),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          ]);
+                        }).toList(),
                       ),
-                    ],
+                    ),
                   ),
                 ),
-                if (userProvider.isLoading)
-                  const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: CircularProgressIndicator(),
-                  )
-                else if (_filteredUsers.isEmpty)
-                  const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'No hay usuarios disponibles.',
-                      style: TextStyle(color: Colors.grey),
+              // Paginación
+              if (userProvider.pagination != null)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: _currentPage > 1
+                          ? () => _loadPage(_currentPage - 1)
+                          : null,
                     ),
-                  )
-                else
-                  Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: DataTable(
-                          columns: const [
-                            DataColumn(label: Text("")),
-                            DataColumn(label: Text("Nombre")),
-                            DataColumn(label: Text("Correo")),
-                            DataColumn(label: Text("Teléfono")),
-                            DataColumn(label: Text("Anexo")),
-                            DataColumn(label: Text("Acciones")),
-                          ],
-                          rows: _filteredUsers.map((user) {
-                            return DataRow(cells: [
-                              DataCell(
-                                IconButton(
-                                  icon: const Icon(Icons.add_circle, color: Colors.blue),
-                                  onPressed: () => showInfoModal(context, user),
-                                ),
-                              ),
-                              DataCell(SelectableText(user.name.toString())),
-                              DataCell(SelectableText(user.email.toString())),
-                              DataCell(SelectableText(user.telefono.toString())),
-                              DataCell(SelectableText(user.anexo.toString())),
-                              DataCell(
-                                Row(
-                                  children: [
-                                    if (isAdmin)
-                                      IconButton(
-                                        icon: const Icon(Icons.edit, color: Colors.blue),
-                                        onPressed: () => showEditModal(context, user),
-                                      ),
-                                    if (isAdmin)
-                                      IconButton(
-                                        icon: const Icon(Icons.delete, color: Colors.red),
-                                        onPressed: () =>
-                                            userProvider.deleteUser(user.id!.toInt()),
-                                      ),
-                                  ],
-                                ),
-                              ),
-                            ]);
-                          }).toList(),
-                        ),
-                      ),
+                    Text('Página $_currentPage'),
+                    IconButton(
+                      icon: Icon(Icons.arrow_forward),
+                      onPressed:
+                          _currentPage < userProvider.pagination!.totalPages
+                              ? () => _loadPage(_currentPage + 1)
+                              : null,
                     ),
-                  ),
-                // Paginación
-                if (userProvider.pagination != null)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.arrow_back),
-                        onPressed: _currentPage > 1
-                            ? () => _loadPage(_currentPage - 1)
-                            : null,
-                      ),
-                      Text('Página $_currentPage'),
-                      IconButton(
-                        icon: Icon(Icons.arrow_forward),
-                        onPressed: _currentPage < userProvider.pagination!.totalPages
-                            ? () => _loadPage(_currentPage + 1)
-                            : null,
-                      ),
-                    ],
-                  ),
-              ],
-            ),
-          );
+                  ],
+                ),
+            ],
+          ),
+        );
       },
     );
   }
@@ -224,7 +269,8 @@ class SearchTableState extends State<SearchTable> {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           child: Container(
             padding: const EdgeInsets.all(16),
             width: MediaQuery.of(context).size.width * 0.4,
@@ -237,7 +283,8 @@ class SearchTableState extends State<SearchTable> {
                   children: [
                     const Text(
                       "Información adicional",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                     IconButton(
                       icon: const Icon(Icons.close),
@@ -280,7 +327,8 @@ class SearchTableState extends State<SearchTable> {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           child: Container(
             padding: const EdgeInsets.all(16),
             width: MediaQuery.of(context).size.width * 0.4,
@@ -293,7 +341,8 @@ class SearchTableState extends State<SearchTable> {
                   children: [
                     const Text(
                       "Editar información",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                     IconButton(
                       icon: const Icon(Icons.close),
